2b45dadc32e2c6f80f5d8bccd2fb8472e876cece
Rafael Kitover <rkitover@cpan.org>
2013-02-11 15:31:22 -0500

    initial commit

---
+++ lib/CGI/Carp/StackTrace.pm
@@ -0,0 +1,46 @@
+use utf8;
+package CGI::Carp::StackTrace;
+
+use strict;
+use warnings;
+use 5.008001;
+
+=encoding UTF-8
+
+=head1 NAME
+
+CGI::Carp::StackTrace - abstract goes here
+
+=head1 SYNOPSIS
+
+    use CGI::Carp::StackTrace;
+
+=head1 DESCRIPTION
+
+=cut
+
+# declarations go here
+
+=head1 FUNCTIONS/METHODS
+
+=head2 some_function
+
+=cut
+
+# some_function goes here
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Some::Module>
+
+=back
+
+=head1 AUTHOR
+
+Rafael Kitover <rkitover@cpan.org>
+
+=cut
+
+__PACKAGE__; # End of CGI::Carp::StackTrace
